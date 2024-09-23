b86a3a5c336ec15cff8d20a708c7e334fabb04f9
JingtianWang-28 <144759925+JingtianWang-28@users.noreply.github.com>
2024-09-23 16:46:31 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,6 @@
-function fib(n) {
-    return n;
+function fib(n, fibArray = [0, 1]) {
+    
+    if (n < fibArray.length) return fibArray;
+    fibArray.push(fib(n - 1, fibArray)[n - 1] + fib(n - 2, fibArray)[n - 2]);
+    return fibArray;
 }
